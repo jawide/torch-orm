f1a1595c218119dc2d41a9c9a950601563e0d501
jawide <jawide@qq.com>
2025-02-20 08:55:54 +0800

    ✨ feat: 升级DataStore以及DataAdapter接口

--- packages/core/src/DataStore.ts
+++ packages/core/src/DataStore.ts
@@ -3,17 +3,20 @@ import { Query } from "./Query";
 
 export interface DataStoreOptions {
   adapter: DataAdapter;
+  collection: string;
   idAttribute?: string;
 }
 
-export class DataStore<T extends Record<string, any>> {
-  private adapter: DataAdapter;
-  private collection: string;
+export class DataStore<T> {
+  public adapter: DataAdapter;
+  public collection: string;
+  public idAttribute: string;
 
-  constructor(collection: string, options: DataStoreOptions) {
+  constructor(options: DataStoreOptions) {
     this.adapter = options.adapter;
-    this.collection = collection;
-    this.adapter.idAttribute = options.idAttribute || "id";
+    this.collection = options.collection;
+    this.idAttribute = options.idAttribute || "id";
+    this.adapter.idAttribute = this.idAttribute;
   }
 
   async find(query: Query = {}): Promise<T[]> {
@@ -24,15 +27,32 @@ export class DataStore<T extends Record<string, any>> {
     return this.adapter.create<T>(this.collection, data);
   }
 
-  async update(id: string | number, data: Partial<T>): Promise<T> {
-    return this.adapter.update<T>(this.collection, id, data);
+  async update(query: Query, data: Partial<T>): Promise<T> {
+    return this.adapter.update<T>(this.collection, query, data);
   }
 
-  async delete(id: string | number): Promise<void> {
-    return this.adapter.delete(this.collection, id);
+  async delete(query: Query): Promise<void> {
+    return this.adapter.delete(this.collection, query);
   }
 
   async clear(): Promise<void> {
     return this.adapter.clear(this.collection);
+  }
+
+  async get(id: string | number): Promise<T> {
+    const results = await this.adapter.find<T>(this.collection, {
+      [this.idAttribute]: id,
+    });
+    return results[0];
+  }
+
+  async set(id: string | number, data: T): Promise<T> {
+    return this.adapter.update<T>(
+      this.collection,
+      {
+        [this.idAttribute]: id,
+      },
+      data
+    );
   }
 }
